2b2a4eafd5cb5e504eda097d26e2b262c918a5d0
canistrobrandian <brovkokosty@gmail.com>
2019-05-14 16:07:42 +0300

    ver 1.2

---
+++ Helpers/NotificationScheduleHelper.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+using API_Notification.Models;
+using Timer = System.Timers.Timer;
+
+namespace API_Notification.Helpers
+{
+    public static class NotificationScheduleHelper
+    {
+
+
+        private static Dictionary<int, Timer> dictionaryTimers = new Dictionary<int, Timer>();
+
+        public static void Intialize(NotificationContext context)
+        {
+            NotificationContext db = context;
+
+
+            // Dictionary<int, int> timerList = new Dictionary<int, int>();
+            if (dictionaryTimers != null && dictionaryTimers.Any())
+            {
+
+                foreach (var key in dictionaryTimers.Keys)
+                {
+                    dictionaryTimers[key].Stop();
+                }
+
+            }
+
+            dictionaryTimers = new Dictionary<int, Timer>();
+            var list = db.notifications.Where(not => not.Date >= DateTime.UtcNow)
+                .ToList();
+            foreach (var i in list)
+            {
+                var tempTime = Convert.ToInt32((i.Date - DateTime.UtcNow).TotalMilliseconds);
+                var tempTimer = new System.Timers.Timer(tempTime);
+                tempTimer.Start();
+                tempTimer.Elapsed += (o, e) => //анонимная функция 
+                {
+                    OnTimerTick(i, tempTimer);
+                };
+                dictionaryTimers.Add(i.Id, tempTimer);
+                //var abc = new //анонимная тип 
+                //{
+                //    id = "asd"
+                //};
+                //  лист на ликшенари поменять
+
+
+
+            }
+        }
+
+        public static void AddTimer(Notification not)
+        {
+            if (dictionaryTimers.ContainsKey(not.Id))
+            {
+                throw new InvalidOperationException("Такой таймер уже существует");
+            }
+            var tempTimer = new Timer();
+            var tempTime = Convert.ToInt32((not.Date - DateTime.UtcNow).TotalMilliseconds);
+            tempTimer.Interval = tempTime;
+            tempTimer.Elapsed += (o, e) => //анонимная функция 
+            {
+                OnTimerTick(not, tempTimer);
+            };
+            tempTimer.Start();
+            dictionaryTimers.Add(not.Id, tempTimer);
+
+        }
+
+        static void UpdateTimer(Notification not)
+        {
+            if (!dictionaryTimers.ContainsKey(not.Id))
+            {
+                throw new InvalidOperationException("Такой таймер не существует");
+            }
+            var tempTimer = dictionaryTimers[not.Id];
+            tempTimer.Stop();
+            var tempTime = Convert.ToInt32((not.Date - DateTime.UtcNow).TotalMilliseconds);
+            tempTimer.Interval = tempTime;
+            tempTimer.Elapsed += (o, e) => //анонимная функция 
+            {
+                OnTimerTick(not, tempTimer);
+            };
+            tempTimer.Start();
+
+        }
+
+        static void DeleteTimer(Notification not)
+        {
+            if (!dictionaryTimers.ContainsKey(not.Id))
+                throw new InvalidOperationException("Такой таймер уже существует");
+            dictionaryTimers[not.Id].Stop();
+            dictionaryTimers.Remove(not.Id);
+        }
+
+        private static void OnTimerTick(Notification notification, Timer timer)
+        {
+            timer.Stop();
+            Console.WriteLine($"сработал таймер {notification.Id}");
+        }
+    }
+}
